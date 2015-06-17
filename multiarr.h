0987d847a61fd16c2587cb150b1a67528264a3bf
Joren Heit <jorenheit@gmail.com>
2015-06-17 15:22:48 +0200

    Licence added

--- multiarr.h
+++ multiarr.h
@@ -1,7 +1,23 @@
+/*Author: Joren Heit (jorenheit@gmail.com)
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef MULTIARRAY_H
 #define MULTIARRAY_H
 
-#include <vector>
+#include <vector>    // default implementation
 #include <algorithm> // std::fill
 
 template <typename T, std::size_t D, template <typename...> class Container = std::vector>
@@ -17,8 +33,6 @@ class MultiArray
     std::size_t d_dimensions[D];
 
 public:
-    std::size_t counter{0}; // debug
-
     // Constructor
     template <typename ... Args>
     MultiArray(Args ... args):
@@ -120,8 +134,6 @@ private:
         template <typename Self, typename ... Indices>
         static void assign(Self &self, size_t const (&ranges)[D][2], T const &val, Indices ... indices)
         {
-            self.counter += (ranges[D - 1][1] - ranges[D - 1][0]);
-
             size_t const begin = self.indexMap(indices ..., ranges[D - 1][0]);
             size_t const end = begin + (ranges[D - 1][1] - ranges[D - 1][0]);
             std::fill(&self.d_data[begin], &self.d_data[end], val);
